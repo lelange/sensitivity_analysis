e7ec450254f15e84a45bbef6686ab54bac316948
Annalena Lange <annalena.lange@dlr.de>
2022-09-03 20:34:00 +0200

    add parameter estimation

--- pycode/sensitivity_analysis/inputFactorSpace.py
+++ pycode/sensitivity_analysis/inputFactorSpace.py
@@ -1,6 +1,11 @@
 import openturns as ot
 from datetime import datetime, date
+import numpy as np
+import pandas as pd
+import time
+import pickle
 
+'''
 # Define Comartment names
 compartments = ['Susceptible', 'Exposed', 'Carrier', 'Infected', 'Hospitalized', 'ICU', 'Recovered', 'Dead']
 # Define age Groups
@@ -169,3 +174,340 @@ inputDistribution.setDescription(input_factor_names)
 
 lower_bounds = [coll[i].getA() for i in range(len(input_factor_names))]
 upper_bounds = [coll[i].getB() for i in range(len(input_factor_names))]
+
+'''
+# Define Comartment names
+compartments = ['Susceptible', 'Exposed', 'Carrier', 'Infected', 'Hospitalized', 'ICU', 'Recovered', 'Dead']
+# Define age Groups
+groups = ['0-4', '5-14', '15-34', '35-59', '60-79', '80+']
+# Define population of age groups
+populations = [40000, 70000, 190000, 290000, 180000, 60000] 
+
+days = 100 # number of days to simulate
+start_day = 18
+start_month = 3
+start_year = 2020
+starting_day = (date(start_year, start_month, start_day) - date(start_year, 1, 1)).days
+dt = 0.1
+num_groups = len(groups)
+num_compartments = len(compartments)
+
+
+# define input factors and their distributions
+# todo max_rist_of_infection_from symptomatic -> twice rist_of_infection...
+
+params_not_age_dependent = [
+    "dummy",
+    "incubation_time", 
+    "serial_interval", 
+    "infectious_mild_time", 
+    "hospitalized_to_ICU_time", 
+    "risk_of_infection_from_symptomatic",
+    "max_risk_of_infection_from_symptomatic",
+    "seasonality",
+    "test_and_trace_capacity" # multiplier of #tnt_capacity = np.sum(populations)*100.0/100000*(10.0/7)
+]
+
+
+params_not_age_dependent_category = [
+    "helper",
+    "virus related", 
+    "virus related", 
+    "transition time", 
+    "transition time", 
+    "virus related",
+    "virus related",
+    "virus related",
+    "NPI related"
+]
+
+params_not_age_dependent_short = [
+    "d",
+    r"$t_{\textnormal{inc}}$",
+    r"$t_{\textnormal{serint}}$",
+    r"$T_I^R$",
+    r"$T_H^U$", 
+    r"$\tilde{\beta}_{I, \textnormal{min}}$",
+    r"$\tilde{\beta}_{I, \textnormal{max}}$",
+    r"k",
+    "cap",
+]
+
+dist_not_age_dependent = [
+    ot.Uniform(0, 1),
+    ot.Uniform(4.1, 7.0), # before 5.2
+    ot.Uniform(0.63, 0.87), #0.5 * 2.67 + 0.5 * 5.2; 0.5 * 4.00 + 0.5 * 5.2; before (3.935, 4.6)
+    ot.Uniform(5.6, 8.4), 
+    ot.Uniform(3, 7),  
+    ot.Uniform(0.1, 0.3), 
+    ot.Uniform(0.3, 0.5), 
+    ot.Uniform(-0.3, 0.3), 
+    ot.Uniform(0.8, 1.2)
+]
+
+params_damping = [
+    'NPI_strength_home',
+    'NPI_strength_school',
+    'NPI_strength_work',
+    'NPI_strength_other',
+    #
+    'NPI_start_day_home',
+    'NPI_start_day_school',
+    'NPI_start_day_work',
+    'NPI_start_day_other',
+]
+
+params_damping_category = ["NPI related"]*len(params_damping)
+
+params_damping_short = [
+    r'$r_{\text{home}}$',
+    r'$r_{\text{school}}$',
+    r'$r_{\text{work}}$',
+    r'$r_{\text{other}}$',
+    #
+    r'$t_{\text{home}}^r$',
+    r'$t_{\text{school}}^r$',
+    r'$t_{\text{work}}^r$',
+    r'$t_{\text{other}}^r$',
+]
+
+dist_damping = [
+    ot.Uniform(0.0, 1.0),
+    ot.Uniform(0.0, 1.0),
+    ot.Uniform(0.0, 1.0),
+    ot.Uniform(0.0, 1.0),
+    #
+    ot.Uniform(0.0, days),
+    ot.Uniform(0.0, days),
+    ot.Uniform(0.0, days),
+    ot.Uniform(0.0, days),
+]
+
+params_transition_duration = [   
+    "hospitalized_to_home_time", 
+    "home_to_hospitalized_time",
+    "ICU_to_home_time", 
+    "ICU_to_death_time"
+]
+params_transition_duration_short = [      
+    r"$T_H^R$", 
+    r"$T_I^H$",    
+    r"$T_U^R$", 
+    r"$T_U^D$",
+]
+
+params_transition_duration_ages = [s + f"_{i}" for s in params_transition_duration for i in range(len(groups))]
+
+params_transition_duration_ages_short = [s  for s in params_transition_duration_short for i in range(len(groups))]
+
+params_transition_duration_ages_category = ["transition time"]*len(params_transition_duration_ages)
+
+
+
+dist_transition_duration_ages = [ 
+    # distributions of the parameters given in params_transition_duration list, one line contains the six age groups
+    ot.Uniform(4, 6), ot.Uniform(4, 6), ot.Uniform(5, 7), ot.Uniform(7, 9), ot.Uniform(9, 11), ot.Uniform(13, 17),
+    ot.Uniform(9, 12), ot.Uniform(9, 12), ot.Uniform(9, 12), ot.Uniform(5, 7), ot.Uniform(5, 7), ot.Uniform(5, 7),
+    ot.Uniform(5, 9), ot.Uniform(5, 9), ot.Uniform(5, 9), ot.Uniform(14, 21), ot.Uniform(14, 21), ot.Uniform(10, 15),
+    ot.Uniform(4, 8), ot.Uniform(4, 8), ot.Uniform(4, 8), ot.Uniform(15, 18), ot.Uniform(15, 18), ot.Uniform(10, 12)
+]
+
+# only needed for parameter estimation and if the dataset starts after the beginning of the dynamics
+initial_numbers_comp = [
+    "init_exposed", 
+    "init_carrier", 
+    "init_infected", 
+    #"init_hospitalized", 
+    #"init_ICU", 
+    #"init_recovered",
+    #"init_dead"
+]
+
+initial_numbers_comp_category = ["initial value"]*len(initial_numbers_comp)
+
+initial_numbers_comp_short = [
+    r"$\text{init}_E$", 
+    r"$\text{init}_C$", 
+    r"$\text{init}_I$", 
+    #"init_hospitalized", 
+    #"init_ICU", 
+    #"init_recovered",
+    #"init_dead"
+]
+
+dist_initial_numbers_comp = [
+    ot.Uniform(0, 200), 
+    ot.Uniform(0, 200), 
+    ot.Uniform(120, 130),  
+    #ot.Uniform(0, 1),
+    #ot.Uniform(0, 1),
+    #ot.Uniform(0, 0.1),
+    #ot.Uniform(0, 0.001)
+]
+
+params_transition_probabilities = [ 
+    "infection_probability_from_contact", 
+    "relative_carrier_infectability",
+    "asymptotic_cases_per_infectious", 
+    "hospitalized_cases_per_infectious", 
+    "ICU_cases_per_hospitalized",
+    "deaths_per_ICU", 
+]
+
+params_transition_probabilities_ages = [s + f"_{i}" for s in params_transition_probabilities for i in range(len(groups))]
+
+params_transition_probabilities_ages_category = ["transition probability"]*len(params_transition_probabilities_ages)
+params_transition_probabilities_short = [  
+    r"$\rho$", 
+    r"$\tilde{\beta}_C$",
+    r"$\mu_C^R$", 
+    r"$\mu_I^H$", 
+    r"$\mu_H^U$",
+    r"$\mu_U^D$", 
+]
+params_transition_probabilities_ages_short = [s for s in params_transition_probabilities_short for i in range(len(groups))]
+
+#print(params_transition_probabilities_ages_short)
+
+dist_transition_probabilities_ages = [
+    ot.Uniform(0.02, 0.04), ot.Uniform(0.05, 0.07), ot.Uniform(0.05, 0.07), ot.Uniform(0.05, 0.07), ot.Uniform(0.08, 0.1), ot.Uniform(0.15, 0.2),
+    ot.Uniform(0.5, 1.5), ot.Uniform(0.5, 1.5), ot.Uniform(0.5, 1.5), ot.Uniform(0.5, 1.5), ot.Uniform(0.5, 1.5), ot.Uniform(0.5, 1.5),
+    ot.Uniform(0.2, 0.3), ot.Uniform(0.2, 0.3), ot.Uniform(0.15, 0.25), ot.Uniform(0.15, 0.25), ot.Uniform(0.15, 0.25), ot.Uniform(0.15, 0.25),
+    ot.Uniform(0.006, 0.009), ot.Uniform(0.006, 0.009), ot.Uniform(0.015, 0.023), ot.Uniform(0.049, 0.074), ot.Uniform(0.15, 0.18), ot.Uniform(0.2, 0.25),
+    ot.Uniform(0.05, 0.1), ot.Uniform(0.05, 0.1), ot.Uniform(0.05, 0.1), ot.Uniform(0.1, 0.2), ot.Uniform(0.25, 0.35), ot.Uniform(0.35, 0.45),
+    ot.Uniform(0.0, 0.1), ot.Uniform(0.0, 0.1), ot.Uniform(0.1, 0.18), ot.Uniform(0.1, 0.18), ot.Uniform(0.3, 0.5), ot.Uniform(0.5, 0.7),
+]
+
+input_factor_names = params_not_age_dependent \
+                    + params_damping \
+                    + params_transition_duration_ages \
+                    + params_transition_probabilities_ages \
+                    + initial_numbers_comp
+
+input_factor_names_short = params_not_age_dependent_short \
+                    + params_damping_short \
+                    + params_transition_duration_ages_short \
+                    + params_transition_probabilities_ages_short \
+                    + initial_numbers_comp_short
+
+age_dep = [False for i in params_not_age_dependent] \
+                    + [False for i in params_damping] \
+                    + [True for i in params_transition_duration_ages] \
+                    + [True for i in params_transition_probabilities_ages] \
+                    + [False for i in initial_numbers_comp]
+
+categories = params_not_age_dependent_category \
+            + params_damping_category \
+            + params_transition_duration_ages_category \
+            + params_transition_probabilities_ages_category \
+            + initial_numbers_comp_category
+
+print(len(input_factor_names))
+
+distributions = dist_not_age_dependent \
+    + dist_damping \
+    + dist_transition_duration_ages \
+    + dist_transition_probabilities_ages \
+    + dist_initial_numbers_comp
+
+name_dict = dict(zip(input_factor_names,input_factor_names_short))
+
+group_list = np.zeros(len(input_factor_names))
+group_names = [
+    # compartment_dependent
+    "init", 
+    # location dependent
+    "NPI_strength",
+    "NPI_start",
+    # age dependent
+    "hospitalized_to_home_time",
+    "home_to_hospitalized_time",
+    "ICU_to_home_time",
+    "ICU_to_death_time",
+    "infection_probability_from_contact",
+    "relative_carrier_infectability",
+    "asymptotic_cases_per_infectious",
+    "hospitalized_cases_per_infectious",
+    "ICU_cases_per_hospitalized",
+    "deaths_per_ICU",
+    # other parameters
+    'dummy',
+    'incubation_time',
+    'serial_interval',
+    'infectious_mild_time',
+    'hospitalized_to_ICU_time',
+    'risk_of_infection_from_symptomatic',
+    'seasonality',
+    'test_and_trace_capacity'
+]   
+
+compartment = ["initial_infections"]
+location = [
+    "NPI_strength",
+    "NPI_start",]
+age = [
+      "hospitalized_to_home_time",
+    "home_to_hospitalized_time",
+    "ICU_to_home_time",
+    "ICU_to_death_time",
+    "infection_probability_from_contact",
+    "relative_carrier_infectability",
+    "asymptotic_cases_per_infectious",
+    "hospitalized_cases_per_infectious",
+    "ICU_cases_per_hospitalized",
+    "deaths_per_ICU",  
+    ]
+other = [
+    'dummy',
+    'incubation_time',
+    'serial_interval',
+    'infectious_mild_time',
+    'hospitalized_to_ICU_time',
+    'risk_of_infection_from_symptomatic',
+    'seasonality', 
+    'test_and_trace_capacity'
+    ]      
+
+group_ids = dict(zip(group_names, range(len(group_names))))
+#print(group_ids)
+groups = [name for factor_name in input_factor_names for name in group_names if (name in factor_name) ]
+#print(groups)
+#print(len(groups))
+
+# factor is in which group
+group_dict = {}
+for i in range(len(input_factor_names)):
+    group_dict[input_factor_names[i]] = groups[i]
+    
+problem = {  
+    'names': input_factor_names,
+    'bounds': [[distributions[i].getA(), distributions[i].getB()] for i in range(len(input_factor_names))]}
+
+df_input_factors = pd.DataFrame({"Name": input_factor_names, 
+                   "Short": input_factor_names_short, 
+                   "ot dist": distributions, 
+                   'bounds': [[distributions[i].getA(), distributions[i].getB()] for i in range(len(input_factor_names))],
+                   'lower_bounds': [distributions[i].getA() for i in range(len(input_factor_names))],
+                   'upper_bounds': [distributions[i].getB() for i in range(len(input_factor_names))],
+                   "Age-dep": age_dep, 
+                   "Group": [group_dict[name] for name in input_factor_names],
+                   "Category": categories
+                  })
+
+
+df_fixing = pd.read_csv('results/dgsm_vi_important.csv', index_col = 0)
+df_input_factors['fix_by_vi']=[df_fixing.loc[name, 'vi'] for name in input_factor_names]
+df_input_factors = df_input_factors.set_index('Name', drop = True)
+
+def fix_factors(df_input_factors, column_to_fix = 'fix_by_vi', fixed_value = 'mean'):
+    dict_fixed_factors = {}
+    if fixed_value == 'mean':
+        dict_fixed_factors = df_input_factors[df_input_factors[column_to_fix]].loc[:, 'ot dist'].apply(lambda x : x.getMean()[0]).to_dict()
+    if fixed_value == 'random':
+        df_input_factors[df_input_factors[column_to_fix]].loc[:, 'ot dist'].apply(lambda x : float(np.array(x.getSample(1)))).to_dict()
+    lower_bounds = list(df_input_factors[df_input_factors[column_to_fix] == False].loc[:, 'lower_bounds'].values)
+    upper_bounds = list(df_input_factors[df_input_factors[column_to_fix] == False].loc[:, 'upper_bounds'].values)
+    varying_input_factors = list(df_input_factors[df_input_factors[column_to_fix] == False].index)
+    return dict_fixed_factors, lower_bounds, upper_bounds, varying_input_factors
+
+dict_fixed_factors, lower_bounds, upper_bounds, varying_input_factors = fix_factors(df_input_factors, column_to_fix = 'fix_by_vi', fixed_value = 'mean')
---
+++ pycode/sensitivity_analysis/parameter_estimation.py
@@ -0,0 +1,259 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime, date
+from utils_SA import simulate_model
+import pypesto
+import pypesto.optimize as optimize
+import pypesto.visualize as visualize
+import pypesto.sample as sample
+import pypesto.store as store
+import matplotlib.pyplot as plt
+import time
+import json
+
+from inputFactorSpace import input_factor_names, df_input_factors
+
+path_data = 'data/worldometer_data.txt'
+
+# Define Comartment names
+compartments = ['susceptible', 'exposed', 'carrier', 'infected', 'hospitalized', 'icu', 'recovered', 'dead']
+Compartments = ['Susceptible', 'Exposed', 'Carrier', 'Infected', 'Hospitalized', 'ICU', 'Recovered', 'Dead']
+# Define age Groups
+groups = ['0-4', '5-14', '15-34', '35-59', '60-79', '80+']
+# Define population of age groups
+populations = [40000, 70000, 190000, 290000, 180000, 60000] 
+
+days = 50 # number of days to simulate
+output_index = 3
+
+start_day = 18
+start_month = 3
+start_year = 2020
+starting_day = (date(start_year, start_month, start_day) - date(start_year, 1, 1)).days
+dt = 0.1
+num_groups = len(groups)
+num_compartments = len(compartments)
+
+static_params = {
+    'num_groups': num_groups, 
+    'num_compartments': num_compartments,
+    'populations': populations,
+    'start_day' : (date(start_year, start_month, start_day) - date(start_year, 1, 1)).days,
+    'days' : days,
+    'dt' : dt,
+    # which compartment's value should be outputed?
+    'output_index' : output_index #compartments.index("Dead")
+}
+
+# load data
+data_dict = {}
+with open(path_data) as f:
+    lines = f.readlines()
+    #data_dict.update(lines[0])
+    print(lines[-1])
+    for i in range(len(lines)-1):
+        (key, value) = lines[i].split(":")
+        value = json.loads(value)
+        data_dict[key] = value
+print(data_dict.keys())
+
+start_dataset = 32
+number_of_days = days
+print(data_dict['categories'][start_dataset])
+print(start_year, start_month, start_day)
+
+# when used for a different time period, initial numbers must be matched first. 
+# Saved simulation is from start day 0, therefore no initial numbers in Hospitalized, Infected and Recovered
+# Divide by 100 because of the population size used in the model
+#dataset = np.zeros((number_of_days+1, 2))
+#dataset[:, 0] = np.array(data_dict['currently_infected'][start_dataset:number_of_days+1+start_dataset])/100.0
+#dataset[:, 1] = np.array(data_dict['total_deaths'][start_dataset:number_of_days+1+start_dataset])/100.0
+
+dead_cases = np.array(data_dict['total_deaths'][start_dataset:number_of_days+1+start_dataset])/100.0
+infected_cases = np.array(data_dict['currently_infected'][start_dataset:number_of_days+1+start_dataset])/100.0
+
+# load file for parameter importance and add to input factor dataframe
+df_fixing = pd.read_csv('results/dgsm_vi_important.csv', index_col = 0)
+df_input_factors['fix_by_vi']=[df_fixing.loc[name, 'vi'] for name in input_factor_names]
+#df_input_factors = df_input_factors.set_index('Name', drop = True)
+
+def fix_factors(df_input_factors, column_to_fix = 'fix_by_vi', fixed_value = 'mean'):
+    dict_fixed_factors = {}
+    if fixed_value == 'mean':
+        dict_fixed_factors = df_input_factors[df_input_factors[column_to_fix]].loc[:, 'ot dist'].apply(lambda x : x.getMean()[0]).to_dict()
+    if fixed_value == 'random':
+        df_input_factors[df_input_factors[column_to_fix]].loc[:, 'ot dist'].apply(lambda x : float(np.array(x.getSample(1)))).to_dict()
+    lower_bounds = list(df_input_factors[df_input_factors[column_to_fix] == False].loc[:, 'lower_bounds'].values)
+    upper_bounds = list(df_input_factors[df_input_factors[column_to_fix] == False].loc[:, 'upper_bounds'].values)
+    varying_input_factors = list(df_input_factors[df_input_factors[column_to_fix] == False].index)
+    return dict_fixed_factors, lower_bounds, upper_bounds, varying_input_factors
+
+dict_fixed_factors, lower_bounds, upper_bounds, varying_input_factors = fix_factors(df_input_factors, column_to_fix = 'fix_by_vi', fixed_value = 'mean')
+# assumption that unknown cases are 3 times as much. Match infected numbers to observed.
+dict_fixed_factors['init_exposed'] = infected_cases[0]
+dict_fixed_factors['init_carrier'] = infected_cases[0]
+dict_fixed_factors['init_infected'] = infected_cases[0]
+
+def simulate_model_logParam(theta, 
+                            param_names = varying_input_factors, 
+                            static_params = static_params,
+                            fixed_factors = dict_fixed_factors):
+    '''
+    Call the model simulation for given log-transformed 
+    values (theta) for the parameters listed in param_names. 
+    The static parameters include all other parameters that are not
+    subject to the optimization.
+    '''
+    theta = np.exp(theta)
+    if fixed_factors is None:
+        result = simulate_model({**static_params, **dict(zip(param_names, theta))})
+    else:
+        result = simulate_model({**static_params, **dict(zip(param_names, theta)), **fixed_factors})
+    return result
+
+
+# TODO: change noise model
+def neg_log_likelihood_logParam(theta, measurement = infected_cases):
+    sigma = theta[0]
+    simulation = simulate_model_logParam(theta[1:])
+    simulation = simulation
+    nllh = np.log(2*np.pi*sigma**2)+((measurement-simulation)/sigma)**2
+    nllh = 0.5*sum(nllh)
+    return nllh
+
+def neg_binomial_log_likelihood():
+    '''
+    Discrete distribution that models nb of successes in a sequence of iid Bernoulli trials
+    before r predefinded events (failures) occur. 
+    Suitable if used to model the nb of infected/ pos. tested individuals as the event rate
+    is known but the number of test is usually not.
+    (Bernoulli trials: COVID tests, success: negative test, failure: positive test)
+    r > 0 (int): number of failures until the experiment is stopped
+    p in [0, 1] (float): success probability in each experiment
+    '''
+    return
+
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+
+objective1 = pypesto.Objective(
+    fun=neg_log_likelihood_logParam,
+)
+
+# set upper and lower bound
+#lb = np.array([1e-3] + [50, 25, 10, 10, 1e-3, 1e-3, 1e-3] + [3, 1, 1, 1, 1, 1] + [0.1, 0.6, 0.05, 0.01, 0.1, 0.15, 0.15, 1])
+#ub = np.array([1] + [150, 75, 30, 30, 20, 20, 1] + [7, 15, 15, 15, 15, 15] + [ 0.9, 1.0, 0.5, 0.16, 0.35, 0.4, 0.77, 3])
+
+lb = np.array([1e-5] + lower_bounds)
+lb = np.where(lb <= 0, 1e-9, lb)
+ub = np.array([1] + upper_bounds)#+1e-9 
+
+lb = np.log(lb)
+ub = np.log(ub)
+
+problem1 = pypesto.Problem(objective=objective1, lb=lb, ub=ub)
+
+# create different optimizers
+optimizer_bfgs = optimize.ScipyOptimizer(method="l-bfgs-b")
+optimizer_tnc = optimize.ScipyOptimizer(method="TNC")
+
+# set number of starts
+n_starts = 15
+# save optimizer trace
+history_options = pypesto.HistoryOptions(trace_record=True)
+
+start = time.time()
+# Run optimizaitons for different optimzers
+# result1_bfgs = optimize.minimize(
+#     problem=problem1,
+#     optimizer=optimizer_bfgs,
+#     n_starts=n_starts,
+#     history_options=history_options,
+#     filename=None,
+# )
+
+result1_tnc = optimize.minimize(
+    problem=problem1,
+    optimizer=optimizer_tnc,
+    n_starts=n_starts,
+    history_options=history_options,
+)
+
+end = time.time()
+
+sampler = sample.AdaptiveMetropolisSampler()
+result = sample.sample(
+    problem=problem1,
+    sampler=sampler,
+    n_samples=10000,
+    result=result1_tnc,
+)
+
+fn = 'logs/'+ time.strftime("%Y-%m-%d %H%M%S") + '_optimizer_result_log.hdf5'
+
+# Write result
+store.write_result(
+    result=result1_tnc,
+    filename=fn,
+    problem=True,
+    optimize=True,
+    sample=True,
+)
+
+print("It needed ", end - start, " seconds.")
+#print("BFGS")
+#print((result1_bfgs.optimize_result.as_list()))
+print("TNC")
+print((result1_tnc.optimize_result.as_list()))
+print("Data_____________________________")
+print(result1_tnc.optimize_result.as_list()[0]['x'])
+print(len(result1_tnc.optimize_result.as_list()[0]['x']))
+
+# plot separated waterfalls
+
+fig, ax1 = plt.subplots(1, 1, figsize=(15, 6))
+# ax[0] = visualize.waterfall(result1_bfgs, size=(15, 6))
+visualize.waterfall(result1_tnc, ax = ax1)
+# ax[0].title.set_text('BFGS Waterfall')
+ax1.title.set_text('TNC Waterfall')
+
+
+
+# plot profiles
+#pypesto.visualize.profiles(result1_tnc)
+
+# plot samples
+# pypesto.visualize.sampling_fval_traces(result1_tnc)
+
+plt.show()
+
+best_values = np.exp(result1_tnc.optimize_result.as_list()[0]['x'][1:])
+print("best values: ", best_values)
+static_params['output_index'] = [Compartments.index("Dead"), Compartments.index("Infected"), Compartments.index("Recovered")]
+simulation = simulate_model({**static_params, **dict(zip(varying_input_factors, best_values)), **dict_fixed_factors})
+print(simulation.shape)
+
+datelist = np.array(pd.date_range(datetime(start_year, start_month,
+                        start_day), periods=days, freq='D').strftime('%m-%d').tolist())
+
+tick_range = (np.arange(int(days / 10) + 1) * 10)
+tick_range[-1] -= 1
+fig, ax = plt.subplots()
+ax.plot(simulation[:, 0], 'o-', markersize = 3, label='simulation dead', color="darkorange") 
+ax.plot(simulation[:, 1], 'o-', markersize = 3, label='simulation infected (cumsum)', color="darkgreen") 
+ax.plot(simulation[:, 2], 'o-', markersize = 3, label='simulation recovered', color="navy") 
+ax.plot(dead_cases, 'o-', markersize = 3, label='measurement dead', color = "bisque")
+ax.plot(infected_cases, 'o-', markersize = 3, label='measurement infected', color = "limegreen")
+#ax.plot(recovered_cases, 'o-', markersize = 3, label='measurement recovered', color = "royalblue")
+ax.set_title("Simulated data and measured data")
+ax.set_xticks(tick_range)
+ax.set_xticklabels(datelist[tick_range], rotation=45)
+ax.legend()
+fig.tight_layout
+plt.show()
+fig.savefig('plots/'+ time.strftime("%Y-%m-%d %H%M%S") + '_Plot_best_simulation.pdf')
+plt.close()
+'''
+'''
